e906cd1f948692ffd5a9521e61e9123957e3a280
df2-baszanowsk <df2-baszanowsk@live.uwe.ac.uk>
2023-07-14 20:13:43 +0100

    Add more db stuff and gitignore.

--- TravelBuddy/Models/AppDbContext.cs
+++ TravelBuddy/Models/AppDbContext.cs
@@ -9,6 +9,23 @@ namespace TravelBuddy.Models
         }
 
         public DbSet<Country> Countries { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Guide> Guides { get; set; }
+        public DbSet<Language> Languages { get; set; }
         // Other DbSets...
+        
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Guide>()
+                .HasMany(g => g.Languages)
+                .WithMany(l => l.Guides)
+                .UsingEntity(j => j.ToTable("GuideLanguages"));
+            
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.GuideDetails)
+                .WithOne(g => g.User)
+                .HasForeignKey<Guide>(g => g.UserId);
+        }
     }
 }
